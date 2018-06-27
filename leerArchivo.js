7968cb38d1b20d1596253de38069f03f14cfa5c5
Hector Garay <hemagarayoc@ittepic.edu.mx>
2018-06-27 13:46:17 -0500

    Recorrido de arreglo y imprimir resultado

--- leerArchivo.js
+++ leerArchivo.js
@@ -3,20 +3,24 @@ var fs = require('fs');
 fs.readFile('alumnos.json', 'utf8',(err, data) => {
     if (err) throw err;
 
-    var renglon = data.split("\n")
-    var cadena = ""
-    for(var i=0;i<renglon.length;i++) {
-        var datos = renglon[i].split(",")
-        cadena+='Registro ' + (i+1) + '\n' +
-                'Nombre: '+datos[0] + '\n' +
-                'Correo: '+datos[1] + '\n' +
-                'Genero: '+datos[2] + '\n' +
-                'Calificaciones' + '\n' +
-                'Matematicas: '+datos[3] + '\n' +
-                'Español: '+datos[4] + '\n' +
-                'Promedio: '+datos[5] + '\n' +
-                'Fecha de Nacimiento: '+datos[6] + '\n' +
-                'Activo: '+datos[7] + '\n'
+
+
+    //Lo parseamos para convertirlo en objeto
+    var types = JSON.parse(data);
+
+
+    //Y lo recorremos
+    for(x=0; x<types.length; x++) {
+        console.log(types[x].Nombre);
+        console.log(types[x].email);
+        console.log(types[x].genero);
+        console.log(types[x].calificaciones);
+        console.log(types[x].promedio);
+        console.log(types[x].email);
+        console.log(types[x].fecha_nac);
+        console.log(types[x].activo);
+
     }
-    console.log(cadena)
+
+
   });
